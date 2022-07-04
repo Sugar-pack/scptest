7293294fc8a8e2cfe0bf4a3324fff3596b4544d9
Timur Akhmadiev <Timur_Akhmadiev@epam.com>
2022-07-04 14:31:49 +0400

    Init

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+
+	fmt.Fprintf(w, "hello\n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+func main() {
+
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
+
+	fmt.Println("Server started on port 8090")
+	err := http.ListenAndServe(":8090", nil)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+}
